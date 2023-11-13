4cc3cdf8f9df2807dc971cefd7fd4b6863c4d724
Carlinhos2304 <carlos.mendez22@inacapmail.cl>
2023-11-13 16:41:10 -0300

    actualizaciones

--- app/src/main/java/com/example/seminario/MainInicioEs.java
+++ app/src/main/java/com/example/seminario/MainInicioEs.java
@@ -8,5 +8,5 @@ public class MainInicioEs extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.inicio);
+        setContentView(R.layout.inicio_estudiante);
 }}
