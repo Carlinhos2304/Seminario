cbd3306684d343b509e0e33c941d8a216c3c8a29
Carlinhos2304 <carlos.mendez22@inacapmail.cl>
2023-12-15 23:05:31 -0300

    qr

--- app/src/main/java/com/example/seminario/MainTokenPerdido.java
+++ app/src/main/java/com/example/seminario/MainTokenPerdido.java
@@ -1,23 +1,39 @@
 package com.example.seminario;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainTokenPerdido extends AppCompatActivity {
 
-    private Spinner spinnerPerdido;
     private EditText fechaperdida;
     private EditText mensajeperdida;
+    private Button botonperdido;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.token_perdido);
-        spinnerPerdido=findViewById(R.id.spinnerPerdido);
         fechaperdida=findViewById(R.id.fechaperdida);
+        botonperdido=findViewById(R.id.botonNotificar);
         mensajeperdida=findViewById(R.id.editTextMensajePerdida);
+
+        botonperdido.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // muestra un mensaje de notificacion exitosa
+                Toast.makeText(MainTokenPerdido.this, "Se ha notificado correctamente al Profesor", Toast.LENGTH_SHORT).show();
+
+                // Cierra la actividad actual
+                finish();
+
+
+            }
+        });
 
 }}
